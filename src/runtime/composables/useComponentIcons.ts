38d72d757a72d498334e220a747ddb0affdedc4c
rijenkii <me@rijenkii.tk>
2025-12-24 15:17:33 +0700

    feat(useComponentIcons): add `loadingPosition` parameter

--- src/runtime/composables/useComponentIcons.ts
+++ src/runtime/composables/useComponentIcons.ts
@@ -28,6 +28,14 @@ export interface UseComponentIconsProps {
   /** When `true`, the loading icon will be displayed. */
   loading?: boolean
   /**
+   * Position of the loading icon on the component:
+   * - `leading` -- at the start of the component
+   * - `trailing` -- at the end of the component
+   * - `auto` -- dependant on the value of the `trailing` property
+   * @defaultValue `auto`
+   */
+  loadingPosition?: 'auto' | 'leading' | 'trailing'
+  /**
    * The icon when the `loading` prop is `true`.
    * @defaultValue appConfig.ui.icons.loading
    * @IconifyIcon
@@ -46,7 +54,11 @@ export function useComponentIcons(componentProps: MaybeRefOrGetter<UseComponentI
   const loadingIcon = computed(() => {
     const iconName = props.value.loading ? (props.value.loadingIcon || appConfig.ui.icons.loading) : undefined
 
-    if (!props.value.trailing) {
+    if (props.value.loadingPosition === "leading") {
+      return { leading: iconName }
+    } else if (props.value.loadingPosition === "trailing") {
+      return { trailing: iconName }
+    } else if (!props.value.trailing) {
       return { leading: iconName }
     } else {
       return { trailing: iconName }
